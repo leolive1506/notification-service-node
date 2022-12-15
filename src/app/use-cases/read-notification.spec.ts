1d5830521d514d4b03f5d6739a73d0f09fb52619
Leonardo Lopes <leonardolivelopes2@gmail.com>
2022-12-15 01:01:41 -0300

    read and unread notification

---
+++ src/app/use-cases/read-notification.spec.ts
@@ -0,0 +1,33 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { ReadNotification } from './read-notification';
+
+describe('read notification', () => {
+  it('should be able to read a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const readNotification = new ReadNotification(notificationsRepository);
+
+    const notification = makeNotification();
+
+    await notificationsRepository.create(notification);
+
+    await readNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].readAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to read a non existing notifications', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const readNotification = new ReadNotification(notificationsRepository);
+
+    expect(() => {
+      return readNotification.execute({
+        notificationId: 'non-existing-notification-id',
+      });
+    }).rejects.toThrow('Notification not found');
+  });
+});
